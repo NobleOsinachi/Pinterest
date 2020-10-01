dd6d3df994a0fe0f596217d80e7ca5c8d84aea2f
NobleOsinachi <56831744+NobleOsinachi@users.noreply.github.com>
2020-10-01 07:43:22 +0000

    Create server.js

---
+++ server.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const ejs = require('ejs');
+const ejsMate = require('ejs-mate');
+const morgan = require('morgan');
+const mongoose = require('mongodb');
